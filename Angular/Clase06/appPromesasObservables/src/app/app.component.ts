6377d0aa52987cb20ab81db9347ed5e8ec830649
admin <admin@MacBook-de-admin.local>
2018-08-04 12:13:16 -0500

    Observables parte 1

--- Angular/Clase06/appPromesasObservables/src/app/app.component.ts
+++ Angular/Clase06/appPromesasObservables/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable, Observer } from 'rxjs';
 
 @Component({
 	selector: 'app-root',
@@ -10,7 +11,40 @@ export class AppComponent {
 	nombre: Promise<string>
 
 	ngOnInit() {
-		const promesa1 = new Promise((resolve, reject) => {
+		const objectoObservable: Observable<string> = Observable.create(
+			(observador: Observer<string>) => {
+				setTimeout(() => {
+					observador.next("Llegó el inspector de Luz del Sur")
+				}, 2000)
+
+				setTimeout(() => {
+					observador.next("Llegó el inspector de la municipalidad")
+				}, 5000)
+
+				setTimeout(() => {
+					observador.complete()
+				}, 7000)
+
+				setTimeout(() => {
+					observador.error("Se incendió la casa")
+				}, 8000)
+
+				setTimeout(() => {
+					observador.next("La ventana está abierta")
+				}, 10000)
+			}
+		)
+
+		objectoObservable.subscribe(
+			ftnNext => console.log(ftnNext),
+			ftnError => console.log(ftnError),
+			() => console.log("Se dejó de observar")
+		)
+
+
+
+
+		/*const promesa1 = new Promise((resolve, reject) => {
 			setTimeout(() => {
 				resolve([2018, 2014, 2010])
 			}, 4000)
@@ -28,7 +62,7 @@ export class AppComponent {
 				console.log(data[1])
 				console.log("Trabajo concluido")
 			})
-			.catch(error => console.log(error))
+			.catch(error => console.log(error))*/
 
 	}
 
